b1efce673ac45575ed9a85947a94573af74281bd
Gizmog22 <32348915+Gizmog22@users.noreply.github.com>
2019-03-09 14:07:37 -0600

    Initial commit

---
+++ piplanning_status.php
@@ -0,0 +1,16 @@
+<?php
+
+  $nav_selected = "PIPLANNING";
+  $left_buttons = "YES"; 
+  $left_selected = "STATUS";
+
+  include("./nav.php");
+  global $db;
+
+  ?>
+
+  <img src="images/work_in_progress.jpg" height = "100" width = "100"/>
+  <h3> Status </h3>
+  TBD
+
+<?php include("./footer.php"); ?>
