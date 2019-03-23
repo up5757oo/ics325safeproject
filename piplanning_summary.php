d122794834576c6e77db5160e99643c7162cd7bc
Sala Chang <jk1814ed@metrostate.edu>
2019-03-23 12:22:49 -0500

    remove construction image

--- piplanning_summary.php
+++ piplanning_summary.php
@@ -11,7 +11,6 @@
   ?>
 
   <link rel="stylesheet" type="text/css" href="styleCustom.css">
-  <img src="images/work_in_progress.jpg" height = "100" width = "100"/>
 
 <!--  _______________________________________________________________________ -->
   
