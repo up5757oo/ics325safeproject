b0a77a72fac9776b603ac62fadfb69161570b16e
Sala Chang <jk1814ed@metrostate.edu>
2019-03-23 11:34:29 -0500

    Pi_id Stuff in the works

--- piplanning_summary.php
+++ piplanning_summary.php
@@ -4,14 +4,30 @@
   $left_buttons = "YES"; 
   $left_selected = "SUMMARY";
 
-
   include("./nav.php");
+  include("./db_connection.php");
   global $db;
 
   ?>
 
+  <link rel="stylesheet" type="text/css" href="styleCustom.css">
   <img src="images/work_in_progress.jpg" height = "100" width = "100"/>
-  <h3> Capacity Summary </h3>
+  
+  <h3> Bear's Capacity Summary </h3>
+  <?php
+//uses the pi Select Now function to identify the PI ID within the current date and adds it to the pi id select variable for the default
+$pi_id_select = piSelectNow();
+
+//capturing the pi id cookie to use for the array and BUILD the menu list
+if(isset($_COOKIE['piCookie'])){
+  $pi_id = $_COOKIE['piCookie'];
+  $pi_id_menu = buildPi_idMenu($pi_id);
+} else {
+  $pi_id=$pi_id_select;
+  $pi_id_menu = buildPi_idMenu($pi_id);
+};
+
+?>
   <br> * What is the capacity of each ART in the current PI (PI?)
   <br> * What is the cpacity of each TEAM in the current PI (PI)?
   <br> * What is capacity in each Iteration (I)?
