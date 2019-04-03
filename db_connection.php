1457f24adf75f8c2f0e7d9aec1f36d750a696bb6
Sala Chang <jk1814ed@metrostate.edu>
2019-04-02 19:51:27 -0500

    comments to help read

--- db_connection.php
+++ db_connection.php
@@ -229,9 +229,9 @@ function buildTeamMenu(){
                     $vel = $velocity[$rownum];
                 } else {
                     $vel = $row2["value"];
-                }
+                }   // I need to add a formula somewhere in this display to calculate story points for each row
                 echo
-                    "<tr>
+                    "<tr>   
                         <td id='capacity-table-td' style='font-weight:500;'>" . $row["last_name"] . "</td>
                         <td id='capacity-table-td' style='font-weight:500;'>" . $row["first_name"] . "</td>
                         <td id='capacity-table-td' style='font-weight:500;'>" . $row["role"] . "</td>
