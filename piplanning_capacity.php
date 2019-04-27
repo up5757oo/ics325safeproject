42d22a9025b83cab03f212bad777a8ad26b8dd22
Katrina Brinatte <up5757oo@metrostate.edu>
2019-04-27 12:47:06 -0500

    updating insert

--- piplanning_capacity.php
+++ piplanning_capacity.php
@@ -226,6 +226,28 @@ form for submitting data that will be prepopulated with data from the variables
 
 
   if (isset($_POST['submit0']) && isset($_COOKIE['totalcapCookie'])) {
+    $sql = "SELECT * FROM `capacity` WHERE program_increment='".$program_increment."' AND team_id='".$selected_team."'";
+    $result = $db->query($sql);
+    if ($result->num_rows > 0) {
+      $row = $result->fetch_assoc();
+      }else{
+        $result_id = $db->query("SELECT max(c.id) + 1 FROM capacity c LIMIT 1;");
+        if ($result_id->num_rows > 0) {
+          $sql_sequence = $result_id->fetch_assoc();
+      };
+
+      $result_team = $db->query("SELECT team_name FROM trains_and_teams where team_id='".$selected_team."' LIMIT 1;");
+        if ($result_team->num_rows > 0) {
+          $team_name = $result_team->fetch_assoc();
+      }else{
+        $team_name = $selected_team;
+      }
+      ;
+      $sqlinsert = "INSERT INTO (id, team_id,team_name,program_increment,iteration_1,iteration_2,iteration_3,iteration_4,iteration_5,iteration_6,iteration_P,total) 
+      VALUES (".$sql_sequence.", '".$selected_team."', '".$team_name."','".$program_increment."',0,0,0,0,0,0,0,0);";
+      $result_insert = $db->query($sqlinsert);
+      };
+
     $pi_capacity = $_COOKIE['totalcapCookie'];
     $count_sequence = count($sequenceArray);
     $PI_array = array();
